170e0dbd406c617cea5ec041048cd03083bf5745
n0tpurplx_ <silvertandluccc@gmail.com>
2026-06-04 11:36:23 +0200

    Implement SendMsg function for Discord webhook
    Add SendMsg function to send a message via Discord webhook

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function SendMsg() {
+  fetch("https://discord.com/api/webhooks/1512024929141391520/DWpAYzJJCov3aYdcbpg4HVG9UKyhd4HlRUUIorpw-hx03Oltf3kI5SQdqs6rgOXo_PH8"), {
+    method: "POST"
+    headers: {
+      "Content-Type": "message"
+      "text": "Hello!"
+    }
+  }
+}
